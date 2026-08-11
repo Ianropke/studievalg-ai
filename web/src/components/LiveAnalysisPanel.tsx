ffac51ebb9dbd07264e5a11b464d43f125d74fd8
Ianropke <ianropke@gmail.com>
2026-08-11 21:46:55 +0200

    Ret typeimport i liveanalysepanel

--- web/src/components/LiveAnalysisPanel.tsx
+++ web/src/components/LiveAnalysisPanel.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { useState } from "react";
+import type { FormEvent } from "react";
 
 interface AnalysisCitation {
   source?: string;
